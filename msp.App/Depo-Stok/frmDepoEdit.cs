2956a063431c7ecbfe1521d0ecb9e7f2e8318035
ademaydogdu <ademaydogdu.007@gmail.com>
2020-10-09 04:50:01 +0300

    ++++

---
+++ msp.App/Depo-Stok/frmDepoEdit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Msp.App.Depo_Stok
+{
+    public partial class frmDepoEdit : Form
+    {
+        public frmDepoEdit()
+        {
+            InitializeComponent();
+        }
+    }
+}
