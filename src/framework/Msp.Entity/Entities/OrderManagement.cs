db81cbdd51204303ee194b9fab51073905c3a881
ademaydogdu <ademaydogdu.007@gmail.com>
2020-10-23 23:54:41 +0300

    23.10.20 23:54

---
+++ src/framework/Msp.Entity/Entities/OrderManagement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Msp.Entity.Entities
+{
+   public class OrderManagement
+    {
+
+        [Key]
+        public int oid { get; set; }
+
+        [StringLength(50)]
+        public string oType { get; set; }
+
+        [StringLength(50)]
+        public string oStatus { get; set; }
+
+        public DateTime oDate { get; set; }
+
+        public int oNo { get; set; }
+
+        public int oAmount { get; set; }
+
+        public int oCTransactionNo { get; set; }
+
+        [StringLength(50)]
+        public string oCTransactionTitle { get; set; }
+
+        [StringLength(50)]
+        public string oDescription { get; set; }
+    }
+}
