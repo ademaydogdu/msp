a7fce37cb294221fb6f78dbbe7d625f8eeb5a02c
Dogan Günes <dogangns.98@gmail.com>
2020-11-16 23:12:32 +0300

    .

---
+++ src/framework/Msp.Models/Models/ProductMovementDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Msp.Models.Models
+{
+    public class ProductMovementDTO
+    {
+        public int RecId { get; set; }
+        public int? ProductId { get; set; }
+        public int? Quantity { get; set; }
+        public decimal? Amount { get; set; }
+    }
+}
