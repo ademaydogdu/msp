2aa7b7b147223433ad446ca422ae8fa317ecd3cc
Dogan Günes <dogangns.98@gmail.com>
2021-01-06 21:18:22 +0300

    .

---
+++ src/framework/Msp.Models/Models/Product/ProductPrintRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Msp.Models.Models.Product
+{
+    public class ProductPrintRequest
+    {
+        public int ProductBegin { get; set; }
+        public int ProductEnd { get; set; }
+        public int companyId { get; set; }
+        public int DepotId { get; set; }
+    }
+}
---
+++ src/framework/Msp.Service/Service/Report/ReportService.cs
@@ -0,0 +1,42 @@
+﻿using Msp.Entity.Entities;
+using Msp.Models.Models;
+using Msp.Models.Models.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Msp.Service.Service.Report
+{
+    public class ReportService : BaseService
+    {
+
+        #region EnvanterBilgi
+        public List<ProductDTO> Get_List_EnvanterBilgisi(ProductPrintRequest req)
+        {
+            using (var _db = new MspDbContext())
+            {
+                List<ProductDTO> products = new List<ProductDTO>();
+                products = base.Map<List<Products>, List<ProductDTO>>(_db.products.Where(x => x.PID >= req.ProductBegin && x.PID <= req.ProductEnd).ToList());
+                if (req.DepotId != 0)
+                {
+                    products = products.Where(x => x.PDepotId == req.DepotId).ToList();
+                }
+                if (req.companyId != 0)
+                {
+                    products = products.Where(x => x.PCompanyId == req.companyId).ToList();
+                }
+                return products;
+            }
+        }
+        #endregion
+
+        #region Sale
+
+
+
+
+        #endregion
+    }
+}
