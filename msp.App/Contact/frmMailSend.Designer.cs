598b93ac14936bae299d90cadd738da2ed2f6fff
Dogan Günes <dogangns.98@gmail.com>
2021-01-09 17:15:19 +0300

    .

--- msp.App/Contact/frmMailSend.Designer.cs
+++ msp.App/Contact/frmMailSend.Designer.cs
@@ -28,19 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.memoEdit1 = new DevExpress.XtraEditors.MemoEdit();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.btn_Send = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
             this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.textEdit1);
+            this.layoutControl1.Controls.Add(this.textEdit2);
+            this.layoutControl1.Controls.Add(this.memoEdit1);
+            this.layoutControl1.Controls.Add(this.btn_Send);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.Root;
+            this.layoutControl1.Size = new System.Drawing.Size(483, 401);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // Root
+            // 
+            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.Root.GroupBordersVisible = false;
+            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.layoutControlItem2,
+            this.layoutControlItem3,
+            this.layoutControlItem4});
+            this.Root.Name = "Root";
+            this.Root.Size = new System.Drawing.Size(483, 401);
+            this.Root.TextVisible = false;
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(117, 12);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(354, 20);
+            this.textEdit1.StyleController = this.layoutControl1;
+            this.textEdit1.TabIndex = 4;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.textEdit1;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(463, 24);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(93, 13);
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.Location = new System.Drawing.Point(117, 36);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Size = new System.Drawing.Size(354, 20);
+            this.textEdit2.StyleController = this.layoutControl1;
+            this.textEdit2.TabIndex = 5;
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.textEdit2;
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 24);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(463, 24);
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(93, 13);
+            // 
+            // memoEdit1
+            // 
+            this.memoEdit1.Location = new System.Drawing.Point(117, 60);
+            this.memoEdit1.Name = "memoEdit1";
+            this.memoEdit1.Size = new System.Drawing.Size(354, 303);
+            this.memoEdit1.StyleController = this.layoutControl1;
+            this.memoEdit1.TabIndex = 6;
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.memoEdit1;
+            this.layoutControlItem3.Location = new System.Drawing.Point(0, 48);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(463, 307);
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(93, 13);
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Location = new System.Drawing.Point(12, 367);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(459, 22);
+            this.btn_Send.StyleController = this.layoutControl1;
+            this.btn_Send.TabIndex = 7;
+            this.btn_Send.Text = "simpleButton1";
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // layoutControlItem4
+            // 
+            this.layoutControlItem4.Control = this.btn_Send;
+            this.layoutControlItem4.Location = new System.Drawing.Point(0, 355);
+            this.layoutControlItem4.Name = "layoutControlItem4";
+            this.layoutControlItem4.Size = new System.Drawing.Size(463, 26);
+            this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem4.TextVisible = false;
             // 
             // frmMailSend
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(483, 401);
+            this.Controls.Add(this.layoutControl1);
             this.Name = "frmMailSend";
-            this.Text = "frmMailSend";
+            this.Text = "Mail Gönder";
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup Root;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.MemoEdit memoEdit1;
+        private DevExpress.XtraEditors.SimpleButton btn_Send;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
     }
 }
--- msp.App/Contact/frmMailSend.cs
+++ msp.App/Contact/frmMailSend.cs
@@ -17,5 +17,69 @@ namespace Msp.App.Contact
         {
             InitializeComponent();
         }
+
+        private void btn_Send_Click(object sender, EventArgs e)
+        {
+            //if (string.IsNullOrEmpty(txtfrom.Text))
+            //{
+            //    MessageBox.Show("From is empty!");
+            //    return;
+            //}
+            //if (string.IsNullOrEmpty(txtmessage.Text))
+            //{
+            //    MessageBox.Show("Message is empty!");
+            //    return;
+            //}
+            //if (string.IsNullOrEmpty(txtpassword.Text))
+            //{
+            //    MessageBox.Show("Password is empty!");
+            //    return;
+            //}
+            //if (string.IsNullOrEmpty(txtport.Text))
+            //{
+            //    MessageBox.Show("Port is empty!");
+            //    return;
+            //}
+            //if (string.IsNullOrEmpty(txtsmtp.Text))
+            //{
+            //    MessageBox.Show("Smtp is empty!");
+            //    return;
+            //}
+            //if (string.IsNullOrEmpty(txtto.Text))
+            //{
+            //    MessageBox.Show("To is empty!");
+            //    return;
+            //}
+            //try
+            //{
+            //    MailMessage email = new MailMessage();
+            //    SmtpClient server = new SmtpClient(txtsmtp.Text.Trim());
+            //    email.From = new MailAddress(txtfrom.Text.Trim());
+            //    List<string> toes = txtto.Text.Split(';').ToList();
+            //    foreach (string mail in toes)
+            //    {
+            //        email.To.Add(mail);
+            //    }
+            //    email.BodyEncoding = Encoding.UTF8;
+            //    email.Subject = "CheckMail";
+            //    email.Body = txtmessage.Text;
+            //    server.Port = Convert.ToInt32(txtport.Text);
+            //    server.Credentials = new System.Net.NetworkCredential(txtfrom.Text.Trim(), txtpassword.Text.Trim());
+            //    if (sslCheck.Checked)
+            //    {
+            //        server.EnableSsl = true;
+            //    }
+            //    else
+            //    {
+            //        server.EnableSsl = false;
+            //    }
+            //    server.Send(email);
+            //    MessageBox.Show("Message Sent!");
+            //}
+            //catch (Exception ee)
+            //{
+            //    MessageBox.Show(ee.Message + "\r\n" + ee.InnerException);
+            //}
+        }
     }
 }
